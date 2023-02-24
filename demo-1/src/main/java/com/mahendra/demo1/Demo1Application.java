a5635b9917de6ee469666a393612cd1722ec998d
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2023-02-24 13:06:04 +0530

    Updated with Config

--- demo-1/src/main/java/com/mahendra/demo1/Demo1Application.java
+++ demo-1/src/main/java/com/mahendra/demo1/Demo1Application.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+
 public class Demo1Application {
 
 	public static void main(String[] args) {
--- demo-1/src/main/java/com/mahendra/demo1/rest/AccountResource.java
+++ demo-1/src/main/java/com/mahendra/demo1/rest/AccountResource.java
@@ -1,6 +1,8 @@
 package com.mahendra.demo1.rest;
 
 import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +13,17 @@ import com.mahendra.demo1.models.Account;
 @RequestMapping("/api/accounts")
 public class AccountResource {
 
+	@Value("${interest.rate}")
+	private Double rateOfInterest;
+
+	public Double getRateOfInterest() {
+		return rateOfInterest;
+	}
+
+	public void setRateOfInterest(Double rateOfInterest) {
+		this.rateOfInterest = rateOfInterest;
+	}
+
 	@GetMapping
 	public Account info() {
 		Account acc= new Account();
@@ -18,6 +31,8 @@ public class AccountResource {
 		acc.setCustomerName("Ajay Deol");
 		
 		acc.setOpeningDate(new Date());
+		System.out.println(rateOfInterest);
 		return acc;
+
 	}
 }
