abcb9ef0435f839a20ac79e3f3d4710ca086a66c
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2023-02-24 15:07:50 +0530

    Cleanup ConfigServer and Discovery Server

--- demo-2/src/main/java/com/mahendra/demo2/Demo1Application.java
+++ demo-2/src/main/java/com/mahendra/demo2/Demo1Application.java
@@ -2,15 +2,11 @@ package com.mahendra.demo2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.client.RestTemplate;
 import  org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
-@EnableDiscoveryClient
-
 public class Demo1Application {
 
 	public static void main(String[] args) {
@@ -18,7 +14,6 @@ public class Demo1Application {
 	}
 
 	@Bean
-	@LoadBalanced
 	public RestTemplate rest(){
 		return new RestTemplate();
 	}
--- demo-2/src/main/java/com/mahendra/demo2/rest/CustomerResource.java
+++ demo-2/src/main/java/com/mahendra/demo2/rest/CustomerResource.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
 import com.mahendra.demo2.models.Account;
 
 @RestController
@@ -14,10 +16,17 @@ public class CustomerResource {
 	@Autowired
 	private RestTemplate rest;
 
+	@Value("${service.baseurl}")
+	private String service1;
+
+	public void setService1(String service1) {
+		this.service1 = service1;
+	}
+
 	@GetMapping
 	public Account info() {
 		
-		Account acc = rest.getForObject("http://service-1/api/accounts/", Account.class);
+		Account acc = rest.getForObject(service1+"/api/accounts/", Account.class);
 		return acc;
 	}
 }
